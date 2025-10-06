f78f25bc6c8d7570ba9ba1789330b5b35d989811
Abelsaba06 <abelsabatermunoz@gmail.com>
2025-10-06 17:09:47 +0200

    Estilos de react

---
+++ React_Abel_Sabater/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './estil.css'
+
+export function App() {
+  return (
+      <article>
+        <header>
+          <img src="" alt=""/>
+          <div>
+          <strong>MDksm</strong>
+          <span>@mcdkc</span>
+          </div>
+        </header>
+        <aside>
+        <button>Seguir</button>
+        </aside>
+      </article>
+  )
+}
+
+export default App
