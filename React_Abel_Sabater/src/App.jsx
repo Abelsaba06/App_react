f2d2a608c000297514e8f29b3267c54a598ae636
Abelsaba06 <abelsabatermunoz@gmail.com>
2025-10-06 22:32:04 +0200

    Twitter card casi terminado

--- React_Abel_Sabater/src/App.jsx
+++ React_Abel_Sabater/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './estil.css'
+import './App.css'
+import './index.css'
 import { TwitterFollow } from './TwitterFollow'
-
 export function App() {
   return(
     <>
-      <TwitterFollow key="1" username="abelsaba06" name="Abel Sabater Muñoz" isFollowing="true"/>
-      <TwitterFollow key="2" username="midudev" name="Miguel Ángel Durán" isFollowing="false"/>
-      <TwitterFollow key="3" username="elonmusk" name="Elon Musk" isFollowing="true"/>
-      <TwitterFollow key="4" username="TheRock" name="Dwayne Johnson" isFollowing="false"/>
+      <TwitterFollow username="abelsaba06" name="Abel Sabater Muñoz" isFollowing="true"/>
+      <TwitterFollow username="midudev" name="Miguel Ángel Durán" isFollowing="false"/>
+      <TwitterFollow username="elonmusk" name="Elon Musk" isFollowing="true"/>
+      <TwitterFollow username="TheRock" name="Dwayne Johnson" isFollowing="false"/>
     </>
   )
 }
